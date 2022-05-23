e06ae079e04b03710ad71c057997c5a1959c0a8d
UBC Student <jack007lyz@gmail.com>
2022-05-22 21:15:31 -0700

    Able to search recipes and delete

--- searchRecipe.js
+++ searchRecipe.js
@@ -1,13 +1,16 @@
 // search for a recipe according to the recipeTitle
-let searchingBody = document.getElementById("recipeBody");
-console.log(searchingBody);
-let bodyArray = searchingBody.querySelector('.recipeList');
-let items = bodyArray.querySelector('#listTitle');
-console.log(bodyArray);
-console.log(items);
-// for(let z=0; z<searchingBody.length; z++) {
-//     console.log(123);
-//     console.log(searchingBody[z]);
-//     let searchedTitle = recipeList[i].getElementsByTagName("li")[0].innerHTML;
-//     console.log(searchedTitle);
-// }
+document.getElementById("searchRecipe").addEventListener("click", searchAndClear);
+
+function searchAndClear(){
+    let title = document.getElementById("searchTitle").value;
+    let searchingBody = document.getElementById("recipeBody");
+    let bodyArray = searchingBody.querySelectorAll('.recipeList');
+    for (let i = 0; i < bodyArray.length; i++){
+        let bodyTitle = bodyArray[i].querySelector("#listTitle").innerHTML;
+        if (bodyTitle.includes(title) && title){
+            searchingBody.removeChild(bodyArray[i]);
+        }
+        break;
+    }
+}
+
